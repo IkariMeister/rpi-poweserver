8f027fb2e803257cd30800391a0aedf60a4f6c87
Unknown <jcgseco@gmail.com>
2016-12-18 20:04:37 +0100

    first steps

---
+++ basic_install.sh
@@ -0,0 +1,16 @@
+sudo -s -p <password>
+apt-get update && apt-get upgrade
+apt-get purge "lxc-docker*"
+apt-get purge "docker.io*"
+apt-get update
+apt-get install -y apt-transport-https ca-certificates gnupg2
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+rm /etc/apt/sources.list.d/docker.list
+cat deb https://apt.dockerproject.org/repo debian-jessie main > /etc/apt/sources.list.d/docker.list 
+apt-get update
+apt-cache policy docker-engine
+apt-get install docker-engine
+groupadd docker
+gpasswd -a ${USER} docker
+service docker start
+apt-get install git-core
