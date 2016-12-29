1eb69c3b4ad58bfb1a2b6d8e38ccbc6712dfa8be
Unknown <jcgseco@gmail.com>
2016-12-29 14:42:48 +0100

    nginx image for ARM
    Dockerfile modified to build an nginx image from alpine ARM
    Dockerfile for jenkins added but not ARM

--- basic_install.sh
+++ basic_install.sh
@@ -10,12 +10,14 @@ cd rpi-poweserver
 git remote add rpi-powerserver https://github.com/IkariMeister/rpi-poweserver.git
 git fetch rpi-powerserver
 git checkout rpi-powerserver/develop
-cp docker.list /etc/apt/sources.list.d/docker.list
+sudo cp rpi-powerserver/docker.list /etc/apt/sources.list.d/docker.list
 sudo apt-get update -y
 sudo apt-cache policy docker-engine
 sudo apt-get update
 sudo apt-get install -y docker-engine
-sudo gpasswd -a ${USER} docker
+groupadd docker 
+gpasswd -a ${USER} docker
 sudo service docker start
-docker build -t commons-nginx ./commons/nginx/
+cd rpi-powerserver/commons/nginx
+docker build -t commons-nginx .
 docker run --name nginx -it -p 80:80 -p 443:443 my_nginx &
